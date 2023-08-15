e9c6f9739aed6221c06e622446e47de740e5f35f
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-15 12:53:04 +0530

    completed music generation

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -1,8 +1,100 @@
-import React from 'react'
+"use client"
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { MusicIcon } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
+import { cn } from '@/lib/utils'
+
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form'
+import Heading from '@/components/Heading'
+import { formSchema } from './constants'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { ChatCompletionRequestMessage } from 'openai'
+import { Empty } from '@/components/Empty'
+import { Loader } from '@/components/Loader'
+
 
 const MusicPage = () => {
+  const router = useRouter()
+  const [music, setMusic] = useState<string>()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: '',
+    },
+  });
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+
+    try {
+      setMusic(undefined)
+      const response = await axios.post('/api/music', values)
+      setMusic(response.data)
+      form.reset()
+    } catch (error) {
+      // TODO: OPEN PRO MODEL
+      console.error(error)
+    } finally {
+      router.refresh();
+    }
+  }
+  console.log(music)
   return (
-    <div>MusicPage</div>
+    <div>
+      <Heading 
+        title='Music Generation'
+        description='Turn your ideas into music.'
+        Icon={MusicIcon}
+        iconColor='text-emerald-500'
+        bgColor='bg-emerald-500/10'
+      />
+      <div className='px-4 ld:px-8'>
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}
+              className='roundeed-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2'
+            >
+              <FormField name='prompt' render={({ field }) => (
+                <FormItem className='col-span-12 lg:col-span-10'>
+                  <FormControl className='m-0 p-0'>
+                  <Input {...field} placeholder='Piano solo' 
+                    className='border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent'
+                    disabled={isLoading}
+                    autoComplete='off'
+                  />
+                  </FormControl>
+                </FormItem>
+              )} />
+              <Button type='submit' className='col-span-12 lg:col-span-2 w-full' disabled={isLoading}>
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+
+        <div className='mt-4 space-y-4'>
+          {isLoading && (
+            <div className='p-8 rounded-lg w-full flex items-center justify-center bg-muted'>
+              <Loader />
+            </div>
+          )}
+          {!music && !isLoading && (
+            <Empty label='No music generated.' />
+          )}
+          {music && (
+            <audio controls className='w-full mt-8'>
+              <source src={music} />
+            </audio>
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ app/api/music/route.ts
@@ -0,0 +1,39 @@
+import Replicate from "replicate";
+import { NextResponse } from 'next/server';
+import { auth } from "@clerk/nextjs"
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN!
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt } = body;
+
+    if(!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    
+    if (!prompt) {
+      return new NextResponse("prompt is required", { status: 400 });
+    }
+
+    const response = await replicate.run(
+      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      {
+        input: {
+          prompt_a: prompt
+        }
+      }
+    );
+
+    return new NextResponse(JSON.stringify(response), { status: 200 });
+
+  } catch (error) {
+    console.error("[CONVERSATION_ERROR]",error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
