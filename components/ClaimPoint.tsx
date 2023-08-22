f7759352b631ed7c85b47328f84d1bd2ee1430c3
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-22 22:40:02 +0530

    removed weekly and daily refrehes

--- components/ClaimPoint.tsx
+++ components/ClaimPoint.tsx
@@ -2,8 +2,6 @@
 import { useEffect, useState } from 'react'
 import { Button } from './ui/button';
 import axios from 'axios';
-import { toast } from 'react-hot-toast';
-import { useAuth } from '@clerk/nextjs';
 
 interface ClaimPointProps {
   isDailyRewardAvailable: boolean;
@@ -13,7 +11,6 @@ interface ClaimPointProps {
 export const ClaimPoint = ( {isDailyRewardAvailable} : ClaimPointProps ) => {
   const [mounted, setMounted] = useState(false);
   const [isClaimed, setIsClaimed] = useState(!isDailyRewardAvailable);
-  const userId = useAuth();
 
   useEffect(() => {
     setMounted(true);
@@ -24,19 +21,7 @@ export const ClaimPoint = ( {isDailyRewardAvailable} : ClaimPointProps ) => {
     return null;
   }
   async function onClaimDailyReward() {
-    try {
-      const res = await axios.put('/api/rewards/claim-daily',{credentials: userId});
-      if(res.status === 200) {
-        setIsClaimed(true);
-        toast.success("Daily reward claimed");
-      } else {
-        toast.error("Failed to claim daily reward");
-        setIsClaimed(true);
-      }
-    } catch (error) {
-      setIsClaimed(true);
-      toast.error("Failed to claim daily reward. Internal server error");
-    }
+    setIsClaimed(true);
   }
 
   return ( isClaimed ? <></> :
