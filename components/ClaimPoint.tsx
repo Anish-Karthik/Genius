55af80f97e5675272d18a8bd4b126cf87cef3a67
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-22 22:50:37 +0530

    Testing weekly and daily reward

--- components/ClaimPoint.tsx
+++ components/ClaimPoint.tsx
@@ -2,6 +2,8 @@
 import { useEffect, useState } from 'react'
 import { Button } from './ui/button';
 import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import { useAuth } from '@clerk/nextjs';
 
 interface ClaimPointProps {
   isDailyRewardAvailable: boolean;
@@ -11,17 +13,29 @@ interface ClaimPointProps {
 export const ClaimPoint = ( {isDailyRewardAvailable} : ClaimPointProps ) => {
   const [mounted, setMounted] = useState(false);
   const [isClaimed, setIsClaimed] = useState(!isDailyRewardAvailable);
+  const userId = useAuth();
 
   useEffect(() => {
     setMounted(true);
-    console.log(`${isDailyRewardAvailable} ${mounted} ${isClaimed}`);
   }, []);
 
   if(!mounted || isClaimed) {
     return null;
   }
   async function onClaimDailyReward() {
-    setIsClaimed(true);
+    try {
+      const res = await axios.put('/api/rewards/claim-daily');
+      if(res.status === 200) {
+        setIsClaimed(true);
+        toast.success("Daily reward claimed");
+      } else {
+        toast.error("Failed to claim daily reward");
+        setIsClaimed(true);
+      }
+    } catch (error) {
+      setIsClaimed(true);
+      toast.error("Failed to claim daily reward. Internal server error");
+    }
   }
 
   return ( isClaimed ? <></> :
