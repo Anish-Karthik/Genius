2afb7226c3cd057ab5335ed4aec35889a1360cea
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-23 09:41:41 +0530

    Demo -2

--- app/(dashboard)/(routes)/cron-daily/route.ts
+++
@@ -1,7 +0,0 @@
-import { refreshDailyReward } from "@/lib/rewards";
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-  await refreshDailyReward();
-  return new NextResponse("DONE REFRESHING", { status: 200 });
-}
