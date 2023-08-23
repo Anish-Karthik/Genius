f76e9f5e9eb65fd99c5e43189a08c0f4455a3034
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-23 10:22:41 +0530

    cron demo

--- app/api/cron-daily/route.ts
+++ app/api/cron-daily/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
-export async function GET(req:Request) {
-  return new NextResponse("Hello01", {status: 200});
+export async function GET(req: Request) {
+  return new NextResponse("Hello", {status: 200});
 }
