555ecd7966dedb2554ef0d1871bb9628ba276b1d
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-08-23 10:31:25 +0530

    cron job for daily reward

--- app/api/cron-daily/route.ts
+++ app/api/cron-daily/route.ts
@@ -1,5 +1,7 @@
+import { refreshDailyReward } from "@/lib/rewards";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
+  await refreshDailyReward();
   return new NextResponse("Hello", {status: 200});
 }
