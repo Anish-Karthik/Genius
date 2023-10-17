25010a4f7345f90f994bed9065bc688eb2829faa
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-10-17 18:14:55 +0530

    Revert "Update LandingContent.tsx"
    This reverts commit e07edf943a2637ed2071d3f9fda9af3bae2085da.

--- components/LandingContent.tsx
+++ components/LandingContent.tsx
@@ -4,10 +4,28 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const testimonials = [
   {
-    name: "Bala Vishaal",
-    avatar: "B",
+    name: "Anish Karthik",
+    avatar: "A",
     title: "Software Engineer",
     description: "This is the best application I've ever used!",
+  },
+  {
+    name: "Anandakannan",
+    avatar: "A",
+    title: "Designer",
+    description: "I use this daily for generating new photos!",
+  },
+  {
+    name: "Dharun Prakash",
+    avatar: "D",
+    title: "Developer",
+    description: "This app has changed my life, cannot imagine working without it!",
+  },
+  {
+    name: "Akash Raj",
+    avatar: "A",
+    title: "Manager",
+    description: "The best in class, definitely worth the premium subscription!",
   },
 ];
 
